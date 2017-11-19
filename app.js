14fc442e1d33970642d4cde70bef3709fbb33663
b.figeczky <b.figeczky@yahoo.com>
2017-11-19 13:50:27 -0500

    adding font awsome icons etc.

--- app.js
+++ app.js
@@ -41,16 +41,11 @@ gamesController(app,fs,js2xmlparser,libxslt,bodyParser);
 
 
 
-
-
-
-
-
-
-
-
-
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
+
+
+
+
